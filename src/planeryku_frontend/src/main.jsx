f7f2908749977fe123d5e4e9911d9b48e1c3b215
andreiy14 <andreiyroring@gmail.com>
2024-11-18 21:59:33 +0700

    bypass login

--- src/planeryku_frontend/src/main.jsx
+++ src/planeryku_frontend/src/main.jsx
@@ -4,26 +4,33 @@ import App from "./App";
 import "./index.scss";
 import { AuthClient } from "@dfinity/auth-client";
 
-const init = async () => {
-  const authClient = await AuthClient.create();
-  const handleRender = async (authClient) => {
-    ReactDOM.createRoot(document.getElementById("root")).render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    );
-  };
-  if (await authClient.isAuthenticated()) {
-    handleRender(authClient);
-    return;
-  }
-  authClient.login({
-    identityProvider: "https://identity.ic0.app/#authorize",
-    onSuccess: async () => {
-      handleRender(authClient);
-    },
-    onError: (error) => {
-      console.log("Error logging in", error);
-    },
-  });
-};
+// const init = async () => {
+//   const authClient = await AuthClient.create();
+//   const handleRender = async (authClient) => {
+//     return ReactDOM.createRoot(document.getElementById("root")).render(
+//       <React.StrictMode>
+//         <App />
+//       </React.StrictMode>
+//     );
+//   };
+//   if (await authClient.isAuthenticated()) {
+//     handleRender(authClient);
+//     return;
+//   }
+//   await authClient.login({
+//     identityProvider: "https://identity.ic0.app/#authorize",
+//     onSuccess: async () => {
+//       handleRender(authClient);
+//     },
+//     onError: (error) => {
+//       console.log("Error logging in", error);
+//     },
+//   });
+// };
+
+// init();
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
