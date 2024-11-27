6f807fac8b9e05399f7ea342bc5da8c22bc1f09a
Michael Al Furqon Akram <mcboo.design@gmail.com>
2024-11-27 18:45:43 +0700

    feat: Create new base task ui

--- src/planeryku_frontend/src/components/base/button.jsx
+++
@@ -1,8 +0,0 @@
-export default function Button({icon, text}) {
-  return (
-    <button className="bg-indigo-800 p-3 rounded">
-        {icon}
-        <span>{text}</span>
-    </button>
-  )
-}
--- src/planeryku_frontend/src/main.jsx
+++ src/planeryku_frontend/src/main.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter(
 		{
 			path: "/",
 			element: <ProjectTask />,
-		},
+		}
 	],
 	{
 		future: {
